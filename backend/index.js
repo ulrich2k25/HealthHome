d9bf1c06c9f0a51c9c802570b4aff77a9636c029
simohadassa <simohadassa@gmail.com>
2025-10-23 17:16:31 +0200

    Ajoute de nouveaux fichiers

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,27 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+<<<<<<< Updated upstream
 // Route test
+=======
+// Connexion MySQL locale (WAMP)
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'Idrelle-21',
+  database: 'software'
+});
+
+db.connect((err) => {
+  if (err) {
+    console.error('❌ Erreur de connexion à MySQL :', err);
+  } else {
+    console.log('✅ Connecté à la base de données MySQL locale (WAMP)');
+  }
+});
+
+// Route test principale
+>>>>>>> Stashed changes
 app.get('/', (req, res) => {
   res.send('✅ API HealthHome fonctionne');
 });
